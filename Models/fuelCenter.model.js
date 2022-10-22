cdd8029cdb991e0cbdc2ab119e32de06abfecb68
Lakma Sanjula <64280626+LakmaSanjula-97@users.noreply.github.com>
2022-10-22 23:55:03 +0530

    Fuel Center Model

---
+++ Models/fuelCenter.model.js
@@ -0,0 +1,30 @@
+const  mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const fuelCenterSchema = new Schema({
+
+    fuelCenterId : {
+        type : String,
+        required: true
+    },
+    fuelCenterName : {
+        type : String,
+        required: true
+    },
+    fuelCenterAddress : {
+        type : String,
+        required: true
+    },
+    district : {
+        type : String,
+        requir: true
+    },
+    
+})
+
+//mongodb data table name
+const FuelCenter = mongoose.model("FuelCenter", fuelCenterSchema);
+
+//export the module
+module.exports = FuelCenter;
