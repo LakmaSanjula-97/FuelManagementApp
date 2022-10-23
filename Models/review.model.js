c63cdd894b09708994e70ef7cc39c735802a11ac
Lakma Sanjula <64280626+LakmaSanjula-97@users.noreply.github.com>
2022-10-23 14:13:10 +0530

    review moel

--- Models/review.model.js
+++ Models/review.model.js
@@ -3,22 +3,10 @@ const  mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
-    reviewerId : {
-        type : String,
-        required: true
-    },
-    flueCenterId : {
-        type : String,
-        required: true
-    },
-    flueCenterName : {
-        type : String,
-        required: true
-    },
-    Comment : {
-        type : String,
-        required: true
-    },
+    reviewerId : { type : String, required: true },
+    flueCenterId : { type : String, required: true },
+    flueCenterName : { type : String, required: true },
+    Comment : { type : String, required: true },
 })
 
 const Review = mongoose.model("Review", reviewSchema);
