c286e9d5d56a57c1f2693d0849334eacad3c03f2
Lakma Sanjula <64280626+LakmaSanjula-97@users.noreply.github.com>
2022-10-23 10:51:57 +0530

    vehical route

---
+++ Routes/vehicalOwner.route.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const Owner = require("../Controllers/vehicalOwner.controller");
+
+module.exports = function () {
+    router.post("/create", Owner.addOwner);
+    router.get("/ownerdetails/:ownerName", Owner.getOwnerDetails);
+    router.get("/", Owner.getAllOwners);
+    router.put("/update/:id", Owner.updateOwner);
+    router.delete("/delete/:id", Owner.deleteOwner);
+    
+  return router;
+};
