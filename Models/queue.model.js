83389d3e03d708a153e17f347c89da0192e06cdc
Lakma Sanjula <64280626+LakmaSanjula-97@users.noreply.github.com>
2022-10-23 14:12:31 +0530

    Queue model

--- Models/queue.model.js
+++ Models/queue.model.js
@@ -4,38 +4,15 @@ const Schema = mongoose.Schema;
 
 const queueSchema = new Schema({
 
-    flueCenterName : {
-        type : String,
-        required: true
-    },
-    vehicleId : {
-        type : String,
-        required: true
-    },
-    OwnerName : {
-        type : String,
-        required: true
-    },
-    fuelType : {
-        type : String,
-        required: true
-    },
-    inTime : {
-        type : Date,
-        requir: true
-    },
-    outTime: {
-        type : Date,
-        require: true
-    },
-    waitingTime: {
-        type : Date,
-        require: true
-    }   
+    flueCenterName : { type : Date, required: true },
+    vehicleId : { type : String, required: true },
+    OwnerName : { type : String, required: true },
+    fuelType : { type : String, required: true },
+    inTime : { type : Date, required: true },
+    outTime: { type : Date, required: true },
+    waitingTime:{ type : Date, required: true },
 })
 
-//mongodb data table name
 const Queue = mongoose.model("Queue", queueSchema);
 
-//export the module
 module.exports = Queue;
