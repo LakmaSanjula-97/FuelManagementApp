781c4bb90a3558bb781d3ea4a4e66a9f32c6b5cd
Lakma Sanjula <64280626+LakmaSanjula-97@users.noreply.github.com>
2022-10-23 10:51:29 +0530

    owner model

---
+++ Models/vehicalOwner.model.js
@@ -0,0 +1,23 @@
+const  mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ownerSchema = new Schema({
+
+    ownerName : {
+        type : String,
+        required: true
+    },
+    VechicalId : {
+        type : String,
+        required: true
+    },
+    fuelType : {
+        type : String,
+        required: true
+    }
+})
+
+const Owner = mongoose.model("Owner", ownerSchema);
+
+module.exports = Owner;
