a295a4c51e7f4af70388233aca273ad730668b51
Lakma Sanjula <64280626+LakmaSanjula-97@users.noreply.github.com>
2022-10-23 10:47:24 +0530

    fuel anailability route

---
+++ Routes/fuelAvailability.route.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const FuelAvailabilityController = require("../Controllers/fuelAvailability.controller");
+
+module.exports = function () {
+    router.post("/create", FuelAvailabilityController.addAvailaabilityDetails);
+    router.get("/queuedetails/:flueCenterId", FuelAvailabilityController.getCenterAvailableDetails);
+    router.get("/", FuelAvailabilityController.getAllAvailabilityDetails);
+    router.put("/update/:id", FuelAvailabilityController.updateAvailability);
+    router.delete("/delete/:id", FuelAvailabilityController.deleteAvailabilityDetails);
+   
+  return router;
+};
