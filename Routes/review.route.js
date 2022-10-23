d827b15fcb2b399537d9d0fe6f6ccc06e158713f
Lakma Sanjula <64280626+LakmaSanjula-97@users.noreply.github.com>
2022-10-23 10:51:16 +0530

    review route

---
+++ Routes/review.route.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const Review = require("../Controllers/review.controller");
+
+module.exports = function () {
+    router.post("/create", Review.addReview);
+    router.get("/queuedetails/:flueCenterId", Review.getReviewDetails);
+    router.get("/", Review.getAllReviews);
+    router.put("/update/:id", Review.updateReview);
+    router.delete("/delete/:id", Review.deleteReview);
+    
+  return router;
+};
