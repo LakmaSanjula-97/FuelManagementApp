8dd8f85f68b4568f6641f125280a05c57f77c1f7
Lakma Sanjula <64280626+LakmaSanjula-97@users.noreply.github.com>
2022-10-23 14:14:03 +0530

    review model

--- Models/review.model.js
+++ Models/review.model.js
@@ -6,7 +6,7 @@ const reviewSchema = new Schema({
     reviewerId : { type : String, required: true },
     flueCenterId : { type : String, required: true },
     flueCenterName : { type : String, required: true },
-    Comment : { type : String, required: true },
+    Comment : { type : String, required: true }, 
 })
 
 const Review = mongoose.model("Review", reviewSchema);
