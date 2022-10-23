24ecca939eba5fe2e12df5ec8f7148d29e995bbc
Lakma Sanjula <64280626+LakmaSanjula-97@users.noreply.github.com>
2022-10-23 14:12:19 +0530

    fuel Availablity model

--- Models/fuelAvailability.model.js
+++ Models/fuelAvailability.model.js
@@ -4,30 +4,12 @@ const Schema = mongoose.Schema;
 
 const fuelAvailabilitySchema = new Schema({
 
-    flueCenterId : {
-        type : String,
-        required: true
-    },
-    flueCenterName : {
-        type : String,
-        required: true
-    },
-    PetrolAvailable : {
-        type : String,
-        required: true
-    },
-    Dieselvailable : {
-        type : String,
-        required: true
-    },
-    FinishlTime : {
-        type : String,
-        required: true
-    },
-    ArrivalTime : {
-        type : String,
-        required: true
-    }
+    flueCenterId : { type : String, required: true },
+    flueCenterName : { type : String, required: true },
+    PetrolAvailable : { type : String, required: true },
+    Dieselvailable : { type : String, required: true },
+    FinishlTime : { type : String, required: true },
+    ArrivalTime : { type : String, required: true },
 })
 
 const FuelAvailability = mongoose.model("FuelAvailability", fuelAvailabilitySchema);
