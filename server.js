626a5090ca2e0234f4e938e806ba32e6a4d7d3a2
Lakma Sanjula <64280626+LakmaSanjula-97@users.noreply.github.com>
2022-10-23 10:49:57 +0530

    server js file

--- server.js
+++ server.js
@@ -22,12 +22,18 @@ mongoose.connect(URL, {
     console.log('_MongoDB Connection Success_')
   })
 
-//   const supplierRouter = require("./routes/suppliers");
-//   app.use("/supplier", supplierRouter);
+  const FuelCenter = require("./Routes/fuelCenter.route");
+  const Queue = require("./Routes/queue.route");
+  const FuelAvailability = require("./Routes/fuelAvailability.route");
+  const Review = require("./Routes/review.route");
+  const Owner = require("./Routes/vehicalOwner.route");
 
-//   const itemRouter = require("./routes/items");
-//   app.use("/item", itemRouter);
+  app.use("/fuelCenter", FuelCenter());
+  app.use("/queue", Queue());
+  app.use("/availability", FuelAvailability());
+  app.use("/review", Review());
+  app.use("/owner", Owner());
 
-  app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is up and running at port ${PORT}`)
-  })
+})
